49c8eb3793fefcda2d6755ef725ce0b5f911c52c
cbrumbau <cbrumbau@users.noreply.github.com>
2018-04-04 17:24:26 -0700

    Add files via upload
    Quick test script for multiple imputation

---
+++ amelia_test.R
@@ -0,0 +1,8 @@
+require(Amelia)
+test_set <- read.csv(file="d:/Downloads/3ebc51612e034ff99e8632a228dae430.csv", header=TRUE, sep=",")
+options(warning.length = 8170)
+a.out <- amelia(test_set, m=5, idvars=c("sample_id"),  empri=0.01*nrow(test_set))
+exclude <- c("ufagecomb","f_forigin","f_clinest","f_apgar5","f_rf_pdiab","f_rf_gdiab","f_rf_phyper","f_rf_ghyper","f_rf_ppb","f_rf_ppo","f_rf_cesar","f_rf_ncesar","f_ob_cervic","f_ob_fail","f_ol_rupture","f_ol_precip","f_ol_prolong","f_ld_induct","f_ld_augment","f_ld_steroids","f_ld_antibio","f_ld_chorio","f_ld_mecon","f_ld_fintol","f_ld_anesth","f_md_present","f_md_route","f_md_trial","f_ab_vent","f_ab_vent6","f_ab_nicu","f_ab_surfac","f_ab_antibio","f_ab_seiz","f_ab_inj","f_ca_anen","f_ca_menin","f_ca_heart","f_ca_hernia","f_ca_ompha","f_ca_gastro","f_ca_limb","f_ca_cleftlp","f_ca_cleft","f_ca_downs","f_ca_chrom","f_ca_hypos","f_wtgain","f_mpcb","f_urf_diabetes","f_urf_chyper","f_urf_phyper","f_urf_eclamp","f_uob_induct","f_uld_meconium","f_uld_precip","f_uld_breech","f_u_forcep","f_u_vacuum","f_uca_anen","f_uca_spina","f_uca_omphalo","f_uca_cleftlp","f_uca_downs","rf_phyp","rf_ghyp","rf_eclam","ld_mecon","ca_anen","ca_menin","ca_ompha","ca_gastro","ca_cleft")
+a.out <- amelia(test_set[, !(names(test_set) %in% exclude)], m=5, idvars=c("sample_id"), empri=0.01*nrow(test_set))
+save(a.out, file = "imputations.RData")
+write.amelia(obj=a.out, file.stem = "outdata")
