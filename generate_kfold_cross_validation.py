3235e337bb04376235723dcdd675fd1f1039f6e9
cbrumbau <cbrumbau@users.noreply.github.com>
2018-04-13 15:30:28 -0700

    Add files via upload

--- generate_kfold_cross_validation.py
+++ generate_kfold_cross_validation.py
@@ -13,7 +13,7 @@ parser.add_argument('observations_dir', action='store', help='The complete factu
 parser.add_argument('output_dir', action='store', help='The output folder to write the merged files to.')
 args = parser.parse_args()
 
-def merge_csv(csv_files, test_set_csv):
+def merge_csv(csv_files, output_csv):
 	merge_list = list()
 	for file in csv_files:
 		this_csv = merge_dataset.read_csv(os.path.normpath(args.observations_dir + os.sep + file))
@@ -24,7 +24,7 @@ def merge_csv(csv_files, test_set_csv):
 			# Read in the rest of the files without header
 			merge_list.extend(this_csv[1:])
 	# Write out to file
-	csv_file = open(os.path.normpath(args.output_dir + os.sep + test_set_csv), 'w', newline='')
+	csv_file = open(os.path.normpath(args.output_dir + os.sep + output_csv), 'w', newline='')
 	writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
 	for line in merge_list:
 		writer.writerow(line)
