2ab8792f499a274ebeae0832dccd63c5a2b6880a
cbrumbau <cbrumbau@users.noreply.github.com>
2018-04-15 23:01:20 -0700

    Add files via upload

--- generate_random_forests_h2o.R
+++ generate_random_forests_h2o.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 #
-# Rscript generate_random_forests.R --merge imputations_folder output_folder
+# Rscript generate_random_forests_h2o.R imputations_folder output_folder
 # This R script generates random forests from an imputations folder to an output folder as R object files.
 #
 # Chris Brumbaugh, cbrumbau@gmail.com, 04/15/2018
@@ -10,10 +10,6 @@ library("optparse")
 library("tools")
 library("randomForest")
 
-option_list <- list(
-	make_option(c("-m", "--merge"), type="logical", default=FALSE, 
-		help="merge the imputation random forest models by data set and do not compute the random forests [default %default]")
-)
 opt_parser <- OptionParser(usage = "%prog [options] imputations_folder output_folder", option_list=option_list)
 opt <- parse_args(opt_parser, positional_arguments=2)
 
@@ -35,33 +31,7 @@ rforest <- function(filename) {
 	})
  }
 
-if (opt$options$merge[1]) {
-	files <- list.files(path=opt$args[2])
-	# Categorize imputations belonging to each data set from the files
-	files.set = list()
-	for (this.file in files) {
-		this.dataset <- gsub("_\\d+\\.rds", "", this.file, ignore.case=T)
-		if (this.dataset %in% names(files.set)) {
-			files.set[[this.dataset]] <- append(files.set[[this.dataset]], this.file)
-		} else {
-			files.set[this.dataset] <- list(this.file)
-		}
-	}
-	# Read in all the randomForest objects for each data set
-	for (this.dataset in names(files.set)) {
-		models.set = list()
-		for (this.rds in files.set[[this.dataset]]) {
-			models.set[[length(models.set)+1]] <- readRDS(paste(opt$args[2], this.rds, sep=""))
-		}
-		# Merge into one model and write this output
-		print(paste("Merging random forest models for ", this.dataset))
-		merged.rf <- do.call("combine", models.set)
-		print("Saving merged model...")
-		saveRDS(merged.rf, file=paste(paste(opt$args[2], this.dataset, sep=""), ".rds", sep=""))
-	}
-} else {
-	localH2O <- h2o.init()
-	files <- list.files(path=opt$args[1])
-	invisible(lapply(files, rforest))
-	h2o.shutdown(prompt=FALSE)
-}
+localH2O <- h2o.init()
+files <- list.files(path=opt$args[1])
+invisible(lapply(files, rforest))
+h2o.shutdown(prompt=FALSE)
