f62159d1d30154bce3bfef3ecfddb5dabea72ab2
cbrumbau <cbrumbau@users.noreply.github.com>
2018-04-19 22:21:19 -0700

    Add files via upload

---
+++ generate_rlm_caret.R
@@ -0,0 +1,79 @@
+#!/usr/bin/env Rscript
+#
+# Rscript generate_rlm_caret.R --folds --include --merge imputations_folder output_folder
+# This R script generates robust linear regression models from an imputations folder to an output folder as R object files.
+#
+# Chris Brumbaugh, cbrumbau@gmail.com, 04/17/2018
+
+library("caret")
+library("doMC")
+library("optparse")
+library("tools")
+
+option_list <- list(
+	make_option(c("-f", "--folds"), type="integer", default=5,
+		help="number of folds to use for cross validation [default %default]", metavar="number"),
+	make_option(c("-i", "--include"), type="character", default="", 
+		help="file containing specific features to use for training the elasticnet [default %default]"),
+	make_option(c("-m", "--merge"), type="logical", default=FALSE, 
+		help="merge the imputation elasticnet models by data set and do not compute the models [default %default]")
+)
+opt_parser <- OptionParser(usage = "%prog [options] imputations_folder output_folder", option_list=option_list)
+opt <- parse_args(opt_parser, positional_arguments=2)
+registerDoMC()
+
+if (nchar(opt$options$include[1]) > 0) {
+	include <- scan(opt$options$include[1], what=character())
+}
+
+splitbyset <- function(filelist) {
+	byset <- list()
+	for (this.file in filelist) {
+		this.file.noext <- tools::file_path_sans_ext(this.file)
+		this.dataset <- gsub("_\\d+", "", this.file.noext, ignore.case=T)
+		if (this.dataset %in% names(byset)) {
+			byset[[this.dataset]] <- append(byset[[this.dataset]], this.file)
+		} else {
+			byset[this.dataset] <- list(this.file)
+		}
+	}
+	return(byset)
+}
+
+enet <-function(dataset.name, imputation.list) {
+	print(paste("Processing ", dataset.name, sep=""))
+	imputed.set <- list()
+	for (file in imputation.list) {
+		imputed.set[[length(imputed.set)+1]] <- read.csv(file=paste(opt$args[1], file, sep=""), header=TRUE, sep=",")
+	}
+	imputed.x <- list()
+	imputed.y <- list()
+	for (this.set in imputed.set) {
+		imputed.x[[length(imputed.x)+1]] <- this.set[, !names(this.set) %in% c("X", "sample_id", "z", "y")]
+		if (nchar(opt$options$include[1]) > 0) {
+			imputed.x[[length(imputed.x)]] <- this.set[, include]
+		}
+		imputed.y[[length(imputed.y)+1]] <- this.set[, c("y")]
+	}
+	train.control <- trainControl(method="cv", number=opt$options$folds[1], verboseIter=TRUE)
+	print(paste("Generating rlm for", imputation.list, sep=" "))
+	start.time <- Sys.time()
+	en.list <- foreach(this.x=imputed.x, this.y=imputed.y, .inorder=TRUE, .packages='caret') %dopar% {
+		train(x=this.x, y=this.y, trControl=train.control, method="rlm")
+	}
+	print(Sys.time()-start.time)
+	print("Saving models...")
+	for (i in 1:length(en.list)) {
+		saveRDS(en.list[[i]], file=paste(opt$args[2], tools::file_path_sans_ext(imputation.list[i]), ".rds", sep=""))
+	}
+}
+
+if (opt$options$merge[1]) {
+	print("TO BE COMPLETED")
+} else {
+	files <- list.files(path=opt$args[1])
+	files.set <- splitbyset(files)
+	for (this.dataset in names(files.set)) {
+		enet(this.dataset, files.set[[this.dataset]])
+	}
+}
--- random_forest_feature_selection.R
+++ random_forest_feature_selection.R
@@ -21,7 +21,9 @@ opt <- parse_args(opt_parser, positional_arguments=2)
 this.set <- read.csv(file=opt$args[1], header=TRUE, sep=",")
 
 # Perform the feature selection
+start.time <- Sys.time()
 result.list <- rfcv(this.set[, !names(this.set) %in% c("X", "sample_id", "z", "y")], this.set[, c("y")], cv.fold=opt$options$folds[1], step=opt$options$step[1])
+print(Sys.time()-start.time)
 
 # Save to file
 saveRDS(result.list, file=opt$args[2])
