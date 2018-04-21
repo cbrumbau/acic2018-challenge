53a401f37118d2c652190b77b9c366026c5839dd
cbrumbau <cbrumbau@users.noreply.github.com>
2018-04-20 19:58:25 -0700

    Add files via upload

--- predict_using_random_forests.R
+++ predict_using_random_forests.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 #
-# Rscript predict_using_random_forests.R --threads rforest_model.rds exclude_file test_folder output_folder
+# Rscript predict_using_random_forests.R --include --exclude --threads rforest_model.rds test_folder output_folder
 # This R script generates predictions using a random forest model file from a test set folder to an output folder as csv files.
 #
 # Chris Brumbaugh, cbrumbau@gmail.com, 04/16/2018
@@ -11,14 +11,24 @@ library("tools")
 library("randomForest")
 
 option_list <- list(
+	make_option(c("-e", "--exclude"), type="character", default="", 
+		help="file containing specific features to exclude, cannot be used with include [default %default]"),
+	make_option(c("-i", "--include"), type="character", default="", 
+		help="file containing specific features to include, cannot be used with exclude [default %default]"),
 	make_option(c("-t", "--threads"), type="integer", default=4,
 		help="number of threads to use for parallel predictions to run on the test set(s) [default %default]")
 )
-opt_parser <- OptionParser(usage = "%prog [options] rforest_model.rds exclude_file test_folder output_folder", option_list=option_list)
-opt <- parse_args(opt_parser, positional_arguments=4)
+opt_parser <- OptionParser(usage = "%prog [options] rforest_model.rds test_folder output_folder", option_list=option_list)
+opt <- parse_args(opt_parser, positional_arguments=3)
 registerDoMC()
 
-# Read in the model and exclude files
+if (nchar(opt$options$exclude[1]) > 0) {
+	exclude <- scan(opt$options$exclude[1], what=character())
+} else if (nchar(opt$options$include[1]) > 0) {
+	include <- scan(opt$options$include[1], what=character())
+}
+
+# Read in the model
 print("Reading in model...")
 this.rf <- readRDS(opt$args[1])
 exclude <- scan(opt$args[2], what=character())
@@ -31,16 +41,22 @@ for (files.list in split(test.files, ceiling(seq_along(test.files)/opt$options$t
 	for (file in files.list) {
 		test.set[[length(test.set)+1]] <- read.csv(file=paste(opt$args[3], file, sep=""), header=TRUE, sep=",")
 	}
-	excluded.x <- list()
+	x.list <- list()
 	for (this.set in test.set) {
-		this.set <- this.set[, !names(this.set) %in% exclude]
-		excluded.x[[length(excluded.x)+1]] <- this.set[, !names(this.set) %in% c("X", "sample_id", "z", "y")]
+		if (nchar(opt$options$exclude[1]) > 0) {
+			this.set <- this.set[, !names(this.set) %in% exclude]
+		} else if (nchar(opt$options$include[1]) > 0) {
+			this.set <- this.set[, include]
+		}
+		x.list[[length(x.list)+1]] <- this.set[, !names(this.set) %in% c("X", "sample_id", "z", "y")]
 	}
 	print(paste("Processing", files.list, sep=" "))
 	# Predict the files in parallel
-	system.time(predict.rf <- foreach(test.data=excluded.x, .inorder=TRUE, .packages='randomForest') %dopar% {
+	start.time <- Sys.time()
+	predict.rf <- foreach(test.data=x.list, .inorder=TRUE, .packages='randomForest') %dopar% {
 		predict(this.rf, test.data)
-	})
+	}
+	print(Sys.time()-start.time)
 	# Write the results to file
 	print("Writing predicted values to file...")
 	for (i in 1:length(predict.rf)) {
