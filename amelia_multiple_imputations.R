9f65de77cd10e8a2edc2c49494feaac85142ff42
cbrumbau <cbrumbau@users.noreply.github.com>
2018-04-22 00:58:58 -0700

    Add files via upload

--- amelia_multiple_imputations.R
+++ amelia_multiple_imputations.R
@@ -5,42 +5,42 @@
 #
 # Chris Brumbaugh, cbrumbau@gmail.com, 04/04/2018
 
+library("Amelia")
 library("optparse")
 library("tools")
-library("Amelia")
 
 option_list <- list(
-	make_option(c("-e", "--exclude"), type="character", default="", 
-		help="folder to write text files containing any colinear variables that were excluded from multiple imputations [default %default]"),
 	make_option(c("-i", "--imputations"), type="integer", default=5, 
 		help="number of imputations to generate [default %default]", metavar="number")
 )
 opt_parser <- OptionParser(usage = "%prog [options] observations_folder output_folder", option_list=option_list)
 opt <- parse_args(opt_parser, positional_arguments=2)
 
-mimput <- function(filename) {
+mimput <- function(filename, exclude=list()) {
 	print(paste("Processing ", filename, sep=""))
 	this.set <- read.csv(file=paste(opt$args[1], filename, sep=""), header=TRUE)
-	result <- tryCatch({
-		start.time <- Sys.time()
-		a.out <- amelia(this.set, m=opt$options$number[1], idvars=c("sample_id"), empri=0.01*nrow(this.set))
-		print(Sys.time()-start.time)
-	}, warning = function(w) {
-		message(paste("WARNING: ", w))
-		# Try again, with warnings removed from the columns
-		exclude <- strsplit(gsub("simpleWarning in amcheck\\(x = x, m = m, idvars = numopts\\$idvars, priors = priors, : |The variable |is perfectly collinear with another variable in the data.\n|\n", "", w), " ")
-		if (nchar(opt$options$exclude[1]) > 0) {
-			write.table(exclude, file=paste(opt$options$exclude[1], tools::file_path_sans_ext(filename), "_exclude.txt", sep=""), row.names=FALSE, col.names=FALSE)
-		}
-		this.set <- this.set[, !(names(this.set) %in% exclude)]
-		start.time <- Sys.time()
-		a.out <- amelia(this.set, m=opt$options$number[1], idvars=c("sample_id"), empri=0.01*nrow(this.set))
-		print(Sys.time()-start.time)
-	}, error = function(e) {
-		message(paste("ERROR: ", e))
-	}, finally = {
-		write.amelia(obj=a.out, file.stem=paste(paste(opt$args[2], tools::file_path_sans_ext(filename), sep=""), "_", sep=""), quote=FALSE, row.names=FALSE)
-	})
+	a.out <- NULL
+	attempt <- 1
+	exclude <- c()
+	while (is.null(a.out) && attempt <= 5) {
+		attempt <- attempt + 1
+		this.set <- this.set[, !names(this.set) %in% exclude]
+		result <- tryCatch({
+			start.time <- Sys.time()
+			a.out <- amelia(this.set, m=opt$options$imputations[1], idvars=c("sample_id"), empri=0.01*nrow(this.set))
+			print(Sys.time()-start.time)
+		}, warning = function(w) {
+			message(paste("WARNING: ", w))
+			# Try again, with warnings removed from the columns
+			exclude <<- unlist(c(exclude, strsplit(gsub("simpleWarning in amcheck\\(x = x, m = m, idvars = numopts\\$idvars, priors = priors, : |The variable |is perfectly collinear with another variable in the data.\n|\n", "", w), " ")))
+		}, error = function(e) {
+			message(paste("ERROR: ", e))
+		}, finally = {
+			if (!is.null(a.out)){
+				write.amelia(obj=a.out, file.stem=paste(paste(opt$args[2], tools::file_path_sans_ext(filename), sep=""), "_", sep=""), quote=FALSE, row.names=FALSE)
+			}
+		})
+	}
  }
  
 files <- list.files(path=opt$args[1])
